3e6b247879dc7d58f6786721a3a431a3df3d004b
José Yoshiriro <jyoshiriro@gmail.com>
2021-06-11 21:01:08 -0300

    estrutura inicial do projeto

---
+++ src/main/java/org/jyoshiriro/pocs/marsexplorer/MarsExplorerApplication.java
@@ -0,0 +1,13 @@
+package org.jyoshiriro.pocs.marsexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsExplorerApplication.class, args);
+	}
+
+}
