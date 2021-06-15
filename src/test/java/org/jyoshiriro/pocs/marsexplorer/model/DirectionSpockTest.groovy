e8b27a04a6346d2a7ea09e39421e7199b0c47ffc
José Yoshiriro <jyoshiriro@gmail.com>
2021-06-15 00:25:49 -0300

    Add Spock e testes com Spock p/ Direction

---
+++ src/test/java/org/jyoshiriro/pocs/marsexplorer/model/DirectionSpockTest.groovy
@@ -0,0 +1,59 @@
+package org.jyoshiriro.pocs.marsexplorer.model
+
+import spock.lang.Specification
+
+import static org.jyoshiriro.pocs.marsexplorer.model.Direction.*
+
+class DirectionSpockTest extends Specification {
+
+    def directions = Direction.values()
+
+    def 'All toTheRight and ToTheLeft should be valid'() {
+        when:
+        directions.each { it.getToTheRight() }
+
+        then:
+        noExceptionThrown()
+    }
+
+    def 'Increase should be only 0, -1 or 1'() {
+        when:
+        def validNumbers = [-1, 0, 1]
+
+        then:
+        verifyAll {
+            directions.each {
+                assert validNumbers.contains(it.getIncreaseX())
+                assert validNumbers.contains(it.getIncreaseY())
+            }
+        }
+    }
+
+    def 'All turns to Right should be correct'() {
+        when:
+        def fromToRight = [(N):E, (E):S, (S):W, (W):N]
+
+        then:
+        verifyAll {
+            for (direction in directions) {
+                def actualRight = direction.getToTheRight()
+                def expectedRight = fromToRight[direction]
+                "toRight in $direction: $actualRight" == "toRight in $direction: $expectedRight"
+            }
+        }
+    }
+
+    def 'All turns to Left should be correct'() {
+        when:
+        def fromToLeft = [(N):W, (W):S, (S):E, (E):N]
+
+        then:
+        verifyAll {
+            for (direction in directions) {
+                def actualLeft = direction.getToTheLeft()
+                def expectedLeft = fromToLeft[direction]
+                "toLeft in $direction: $actualLeft" == "toLeft in $direction: $expectedLeft"
+            }
+        }
+    }
+}
