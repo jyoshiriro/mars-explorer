c43f461a50c3e3acc9555555aff53c548260905d
José Yoshiriro <jyoshiriro@gmail.com>
2021-06-14 23:27:34 -0300

    Melhor nome de atr em BoundaryReachedException

--- src/main/java/org/jyoshiriro/pocs/marsexplorer/exception/BoundaryReachedException.java
+++ src/main/java/org/jyoshiriro/pocs/marsexplorer/exception/BoundaryReachedException.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class BoundaryReachedException extends RuntimeException {
 
-    private String boudaryMessage;
+    private String errorMessage;
 
     public BoundaryReachedException(Plane plane, int intendedX, int intendedY) {
-        boudaryMessage = (intendedX >= 0 && intendedY >= 0)
+        errorMessage = (intendedX >= 0 && intendedY >= 0)
                 ? String.format("Plane '%d x %d' can't accept the coordinate (%d,%d)",
                                 plane.getWidth(), plane.getHeight(), intendedX, intendedY)
                 : String.format("X and Y should be 0 or positive. Actual X: %d. Actual Y: %d", intendedX, intendedY);
@@ -18,6 +18,6 @@ public class BoundaryReachedException extends RuntimeException {
 
     @Override
     public String getMessage() {
-        return this.boudaryMessage;
+        return this.errorMessage;
     }
 }
